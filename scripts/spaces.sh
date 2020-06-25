686b455956a6343bb577cf9b2dc28d1525f69a62
Magnús Ólafsson <magnusol93@gmail.com>
2020-06-25 22:15:03 +0200

    wip get icon path

--- scripts/spaces.sh
+++ scripts/spaces.sh
@@ -6,9 +6,17 @@ spaces=`yabai -m query --spaces | jq -r -c '.[]'`
 array="[]"
 for space in $spaces; do
   windowInfo=`jq '.windows[0]' <<< $space | xargs -I{} yabai -m query --windows --window {} 2>/dev/null`
+  echo $applicationName 
+  iconName=`defaults read /Applications/${applicationName}.app/Contents/Info CFBundleIconFile`
+  echo $iconName
+  iconPath='/Applications/'${applicationName}'.app/Contents/Resources/'${iconName}
+  echo $iconPath
+  windowInfo=`echo $windowInfo | jq '. + {"iconPath": "$iconPath" }`
   windowJson='{"windowInfo": '$windowInfo'}'
-  test=` jq -r --argjson windowInfo "$windowJson" --argjson space "$space" -n '$windowInfo + $space' 2>/dev/null ` 
+  applicationName=`jq '.app' <<< $windowInfo | sed 's/"//g' | sed 's/2//g'`
+  test=`jq -r --argjson windowInfo "$windowJson" --argjson space "$space" -n '$windowInfo + $space' 2>/dev/null ` 
   array=`echo $array | jq --argjson test "$test" '. + [$test]'`
 done
 
+echo '-----------------------'
 echo $array
